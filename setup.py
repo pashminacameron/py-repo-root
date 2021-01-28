3d474538b1a0b4fd82e2b25da1d81a7b1badc153
Pashmina (Bendale) Cameron <11311835+pashminacameron@users.noreply.github.com>
2021-01-28 17:50:49 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setuptools.setup(
       name='py-repo-root',
-      version='0.2.0',
+      version='0.3.0',
       license='MIT',
       description='Python utility for cleaner handling of paths',
       long_description=long_description,

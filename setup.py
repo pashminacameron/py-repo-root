a2395ccb90bbccded2e5c8d3ac5b2cb0ba49901d
Pashmina Cameron <pacamero@microsoft.com>
2021-08-14 23:16:14 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setuptools.setup(
     name="py-repo-root",
-    version="2.0.0",
+    version="2.0.1",
     license="MIT",
     description="Python utility for cleaner handling of paths",
     long_description=long_description,
